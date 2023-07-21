b7bbeb411ab9ce2c9864aa26e7cb84d74ee34978
sabthikaa <127173649+sabthikaa@users.noreply.github.com>
2023-07-21 21:15:50 +0530

    first commit

---
+++ MySQL_CRUD/views.py
@@ -0,0 +1,263 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def home(request):
+    return render(request,"home.html")
+
+
+def hip(request):
+    return render(request,"register.html")
+
+def knee(request):
+    return render(request,"knee.html")
+
+
+def shoulder(request):
+    return render(request,"shoulder.html")
+
+def search(request):
+    return render(request,"search.html")
+
+def compound(request):
+    return render(request,"compound.html")
+
+
+def closefracture(request):
+    return render(request,"closefracture.html")
+
+def compartment(request):
+    return render(request,"compartment.html")
+
+def crush(request):
+    return render(request,"crush.html")
+
+def fat(request):
+    return render(request,"fat.html")
+
+def fracture(request):
+    return render(request,"fracture.html")
+
+def myositis(request):
+    return render(request,"myositis.html")
+
+def nerveinjuries(request):
+    return render(request,"nerveinjuries.html")
+
+def pathological(request):
+    return render(request,"pathological.html")
+
+def stress(request):
+    return render(request,"stress.html")
+
+def pyogenic(request):
+    return render(request,"pyogenic.html")
+
+def volk(request):
+    return render(request,"volk.html")
+
+def Trigger(request):
+    return render(request,"Trigger.html")
+
+def tension(request):
+    return render(request,"tension.html")
+
+def Tennis(request):
+    return render(request,"Tennis.html")
+
+def tb(request):
+    return render(request,"tb.html")
+
+
+def supre(request):
+    return render(request,"supre.html")
+
+def spon(request):
+    return render(request,"spon.html")
+
+def sp(request):
+    return render(request,"sp.html")
+
+
+def slipp(request):
+    return render(request,"slipp.html")
+
+def shoul(request):
+    return render(request,"shoul.html")
+
+def crush(request):
+    return render(request,"crush.html")
+
+def scan(request):
+    return render(request,"scan.html")
+
+def rup(request):
+    return render(request,"rup.html")
+
+def ring(request):
+    return render(request,"ring.html")
+
+def recurrent(request):
+    return render(request,"recurrent.html")
+
+def radio(request):
+    return render(request,"radio.html")
+
+def paget(request):
+    return render(request,"paget.html")
+
+def osteo(request):
+    return render(request,"osteo.html")
+
+def ole(request):
+    return render(request,"ole.html")
+
+def monteggia(request):
+    return render(request,"monteggia.html")
+
+def manage(request):
+    return render(request,"manage.html")
+
+def mallet(request):
+    return render(request,"mallet.html")
+
+def ideal(request):
+    return render(request,"ideal.html")
+
+def green(request):
+    return render(request,"green.html")
+
+def gibbus(request):
+    return render(request,"gibbus.html")
+
+def giant(request):
+    return render(request,"giant.html")
+
+def frozen(request):
+    return render(request,"frozen.html")
+
+def de(request):
+    return render(request,"de.html")
+
+
+def fractureofpetalla(request):
+    return render(request,"fractureofpetalla.html")
+
+def external(request):
+    return render(request,"external.html")
+
+def Dupuytren(request):
+    return render(request,"Dupuytren.html")
+
+def colles(request):
+    return render(request,"colles.html")
+
+def codman(request):
+    return render(request,"codman.html")
+
+def clinical(request):
+    return render(request,"clinical.html")
+
+def carpel(request):
+    return render(request,"carpel.html")
+
+def brodie(request):
+    return render(request,"brodie.html")
+
+def bone(request):
+    return render(request,"bone.html")
+
+def bifida(request):
+    return render(request,"bifida.html")
+
+def aval(request):
+    return render(request,"aval.html")
+
+
+def stick(request):
+    return render(request,"stick.html")
+
+def sandbox(request):
+    return render(request,"sandbox.html")
+
+
+def h1(request):
+    return render(request,"h1.html")
+
+
+def hk(request):
+    return render(request,"hk.html")
+
+def boneandjoints(request):
+    return render(request,"boneandjoints.html")
+
+def age(request):
+    return render(request,"age.html")
+
+def bonecysts(request):
+    return render(request,"bonecysts.html")
+
+def upp(request):
+    return render(request,"upp.html")
+
+def low(request):
+    return render(request,"low.html")
+
+def mis(request):
+    return render(request,"mis.html")
+
+def poris(request):
+    return render(request,"poris.html")
+
+def spin(request):
+    return render(request,"spin.html")
+
+def medical(request):
+    return render(request,"medical.html")
+
+def princ(request):
+    return render(request,"princ.html")
+
+def hipfracture(request):
+    return render(request,"hipfracture.html")
+
+
+def hips(request):
+    return render(request,"hips.html")
+
+
+def back(request):
+    return render(request,"back.html")
+
+def causes(request):
+    return render(request,"causes.html")
+
+
+def pain(request):
+    return render(request,"pain.html")
+
+def arth(request):
+    return render(request,"arth.html")
+
+def bur(request):
+    return render(request,"bur.html")
+def fib(request):
+    return render(request,"fib.html")
+def foot(request):
+    return render(request,"foot.html")
+def hand(request):
+    return render(request,"hand.html")
+def ky(request):
+    return render(request,"ky.html")
+def neck(request):
+    return render(request,"neck.html")
+def scol(request):
+    return render(request,"scol.html")
+def soft(request):
+    return render(request,"soft.html")
+def trial(request):
+    return render(request,"trial.html")
+def ortho(request):
+    return render(request,"ortho.html")
+def gto(request):
+    return render(request,"gto.html")
